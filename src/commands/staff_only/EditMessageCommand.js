d6d54fd7f3070534b16a796e6713f8993a8c2a6e
DanielaGC <42395960+DanielaGC@users.noreply.github.com>
2022-09-09 22:14:34 -0300

    feat: Add EditMessageCommand.js

---
+++ src/commands/staff_only/EditMessageCommand.js
@@ -0,0 +1,21 @@
+import { Command } from '../../structures'
+
+export default class EditMessageCommand extends Command {
+  constructor() {
+    super({
+      name: 'editmsg',
+      user_permission: ['ADMINISTRATOR']
+    })
+  }
+
+  run(client, message, args) {
+    if (!args[0]) return message.channel.send('Você esqueceu de colocar o link da mensagem! Como eu vou editar uma mensagem sem saber qual é?')
+    const messageUrl = args[0].trim().split('/').slice(5, 7)
+    if (!message.length > 2) return message.channel.send('Eu não acho que isso seja um link de mensagem...')
+    const channel = message.guild.channels.cache.get(messageUrl[0])
+    if (!args[1]) return message.channel.send('Nada para falar? Eu preciso que você digite o que deseja enviar.')
+    channel.messages.edit(messageUrl[1], { content: args.slice(1).join(' ')).then(() => {
+      message.channel.send('Mensagem editada com sucesso!')
+    })
+  }
+}
