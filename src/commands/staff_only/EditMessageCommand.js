64476617bb0852629c71f4a41e236dff2dfef650
DanielaGC <42395960+DanielaGC@users.noreply.github.com>
2022-09-09 22:52:13 -0300

    Update EditMessageCommand.js

--- src/commands/staff_only/EditMessageCommand.js
+++ src/commands/staff_only/EditMessageCommand.js
@@ -10,11 +10,12 @@ export default class EditMessageCommand extends Command {
 
   run(client, message, args) {
     if (!args[0]) return message.channel.send('Você esqueceu de colocar o link da mensagem! Como eu vou editar uma mensagem sem saber qual é?')
-    const messageUrl = args[0].trim().split('/').slice(5, 7)
-    if (!message.length > 2) return message.channel.send('Eu não acho que isso seja um link de mensagem...')
-    const channel = message.guild.channels.cache.get(messageUrl[0])
+    const messageUrl = args[0]
+    if (!messageUrl.includes('discord.com')) return message.channel.send('Eu não acho que isso seja um link de mensagem...')
+    const ids = messageUrl.trim().split('/').slice(5, 7)
+    const channel = message.guild.channels.cache.get(ids[0])
     if (!args[1]) return message.channel.send('Nada para falar? Eu preciso que você coloque uma mensagem para poder editar!')
-    channel.messages.edit(messageUrl[1], { content: args.slice(1).join(' ') }).then(() => {
+    channel.messages.edit(ids[1], { content: args.slice(1).join(' ') }).then(() => {
       message.channel.send('Mensagem editada com sucesso!')
     })
   }
