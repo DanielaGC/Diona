8537f3203ff883d3caa8fabd30e401644231961d
DanielaGC <42395960+DanielaGC@users.noreply.github.com>
2025-04-03 04:03:27 -0300

    Add new phrases

--- src/structures/RandomPhrases.js
+++ src/structures/RandomPhrases.js
@@ -1,5 +1,14 @@
 const phrases = [
-
+  'Eu vou destruir a industria do vinho!',
+  'Aw, eu falhei... Ugh, de novo... Todo mundo amou minhas bebidas! Gah... Não tente me consolar. E—eu não preciso que me consolem!',
+  'O olho de um gato pode ver todas as presas no que se escondem na escuridão!',
+  'Orelhas de gato podem ouvir até os passos mais macios no chão.',
+  'As patas de um gato escalam até a árvore mais alta de Teyvat.',
+  'O nariz de um gato pode cheirar as pessoas que gostam e são familiares... Ah? Você leu errado! Como eu poderia ter dito uma coisa dessa?',
+  'Eu gosto, não, AMO comer peixe! O peixe que o papai cozinha é o melhor, um purrfeito prato principal! ...Mas comer peixe como tira-gosto, é uma blasfêmia absoluta!',
+  'Cauda de Lagarto, Pimenta, Conchas Estreladas... O que mais eu preciso para criar o Coquetel Mais Nojento de Todos?',
+  'Chuva! Eu odeio chuva! ... Vamos para dentro... Hunf, eu não tenho medo da água, eu só... Odeio quando minhas roupas ficam molhadas.',
+  'Ahhh... Essa esticada foi boa.'
 ]
 
 export {
